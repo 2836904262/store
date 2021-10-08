8cd8f30ca083cf4908c28ff331eeeabbe4f7c5ae
2836904262 <92003286+2836904262@users.noreply.github.com>
2021-10-08 20:23:28 +0800

    Add files via upload

---
+++ 10.08.py
@@ -0,0 +1,52 @@
+import math
+
+# 华氏温度转换为摄氏温度
+# num=int(input("请输入你的华氏度体温："))
+# print("你的摄氏度体温是",(num-32)/1.8)
+
+# 输入圆的半径，求周长
+# radius=float(input("请输入半径："))
+# print("周长等于",3.14*radius)
+# print("半径等于",3.14*radius*radius)
+
+# 判断年份是不是闰年
+# year=int(input("请输入年份:"))
+# if year%4 == 0 and year%100==0:
+#     print("这是个世纪闰年")
+# elif year%4 == 0:
+#     print("这是个普通闰年")
+# else:
+#     print("这是平年")
+
+# 英寸与厘米互换
+# length=float(input("请输入英寸长度："))
+# print("换算成厘米是",length*2.54,"cm")
+
+# 百分制成绩转换为等级制成绩
+# grade=float(input("请输入分数："))
+# if grade>=90:
+#     print("A")
+# elif grade>=80 and grade<90:
+#     print("B")
+# elif grade>=70 and grade<80:
+#     print("C")
+# elif grade>=60 and grade<70:
+#     print("D")
+# elif grade<60:
+#     print("E")
+# else:
+#     print("输入错误")
+
+# 根据三条边长判断能否组成三角形，可以求其面积与周长
+# length1=float(input("请输入第一条线长："))
+# length2=float(input("请输入第二条线长："))
+# length3=float(input("请输入第三条线长："))
+#
+# if length1+length2>length3 and length1+length3>length2 and length2+length3>length1 :
+#     grith = length1+length2+length3
+#     gr = grith/2
+#     area = math.sqrt(gr*(gr-length1)*(gr-length2)*(gr-length3))
+#     print("可以生成三角形，周长为",grith,"面积为",area)
+# else:
+#     print("不可以生成三角形")
+
